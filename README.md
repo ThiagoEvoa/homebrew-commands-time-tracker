9c770510ee010bf0df4445ca1effae269b6582fa
Evoá <thiagoevoa@gmail.com>
2026-03-01 14:04:05 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # homebrew-commands-time-tracker
+
+A universal command time tracker.
+
+## Installation
+
+```bash
+brew tap ThiagoEvoa/commands-time-tracker
+brew install commands-time-tracker
+```
+
+## Configuration
+
+To start tracking your commands, add the following line to your `.zshrc`:
+
+```bash
+source $(brew --prefix commands-time-tracker)/share/commands-time-tracker/time_tracker.sh
+```
+
+## Usage
+
+Use the `summarize` command to view your time tracking summary.
---
+++ formula/commands-time-tracker.rb
@@ -0,0 +1,24 @@
+class CommandsTimeTracker < Formula
+  desc "A universal command time tracker"
+  homepage "https://github.com/ThiagoEvoa/commands_time_tracker"
+  url "https://github.com/ThiagoEvoa/commands_time_tracker/archive/v1.0.0.tar.gz"
+
+  license "MIT"
+
+  def install
+    bin.install "bin/summarize.zsh" => "summarize"
+    pkgshare.install "bin/time_tracker.sh"
+  end
+
+  def caveats
+    <<~EOS
+      To start tracking your commands, add the following line to your .zshrc:
+      
+        source #{opt_pkgshare}/time_tracker.sh
+    EOS
+  end
+
+  test do
+    assert_predicate bin/"summarize", :exist?
+  end
+end
